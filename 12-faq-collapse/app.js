7a48d4713943bf33762ef5c39e56f8c3c9977dfa
Aleksandar Ristic <storiaca@gmail.com>
2021-01-25 13:12:09 +0100

    Random choise pocker start

--- 12-faq-collapse/app.js
+++ 12-faq-collapse/app.js
@@ -2,8 +2,6 @@ const toggleButtons = document.querySelectorAll(".faq-toggle");
 
 toggleButtons.forEach((button) => {
   button.addEventListener("click", function () {
-    console.log(this.parentNode);
-    button.parentNode.classList.remove("active");
     button.parentNode.classList.toggle("active");
   });
 });
