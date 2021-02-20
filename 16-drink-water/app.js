fc03dd8497a32a4d93ff3d319d2a328fc996f221
Aleksandar Ristic <storiaca@gmail.com>
2021-02-20 12:23:59 +0100

    Fill small cups

--- 16-drink-water/app.js
+++ 16-drink-water/app.js
@@ -1,1 +1,24 @@
 const smallCups = document.querySelectorAll(".cup-small");
+const liters = document.getElementsByName("liters");
+const percentage = document.getElementsByName("percentage");
+const remained = document.getElementsByName("remained");
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => highlightCups(idx));
+});
+
+function highlightCups(idx) {
+  if (
+    smallCups[idx].classList.contains("full") &&
+    !smallCups[idx].nextElementSibling.classList.contains("full")
+  ) {
+    idx--;
+  }
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+}
