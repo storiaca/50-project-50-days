a6d411d7feb1c889f787d64811b9b2ae8b748c93
Aleksandar Ristic <storiaca@gmail.com>
2021-03-15 17:01:00 +0100

    js start

---
+++ 18-background-slider/app.js
@@ -0,0 +1,10 @@
+const body = documet.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+let activeSlide = 0;
+
+function setBgToBody() {
+  body.style.backgroundImage;
+}
