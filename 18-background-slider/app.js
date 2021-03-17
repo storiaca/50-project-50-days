210bc5257c1b8337236ce682344313c34bf77bc1
Aleksandar Ristic <storiaca@gmail.com>
2021-03-17 16:58:47 +0100

    Background slider finshed

--- 18-background-slider/app.js
+++ 18-background-slider/app.js
@@ -1,10 +1,52 @@
-const body = documet.body;
+const body = document.body;
 const slides = document.querySelectorAll(".slide");
 const leftBtn = document.getElementById("left");
 const rightBtn = document.getElementById("right");
 
 let activeSlide = 0;
 
+if (activeSlide === 0) {
+  leftBtn.style.display = "none";
+}
+
+rightBtn.addEventListener("click", () => {
+  activeSlide++;
+  if (activeSlide > 0) {
+    leftBtn.style.display = "block";
+  }
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+    rightBtn.style.display = "none";
+  }
+
+  setBgToBody();
+  setActiveSlide();
+});
+
+leftBtn.addEventListener("click", () => {
+  activeSlide--;
+
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1;
+    rightBtn.style.display = "block";
+  }
+
+  if (activeSlide === 0) {
+    leftBtn.style.display = "none";
+  }
+
+  setBgToBody();
+  setActiveSlide();
+});
+
+setBgToBody();
+
 function setBgToBody() {
   body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => slide.classList.remove("active"));
+
+  slides[activeSlide].classList.add("active");
 }
