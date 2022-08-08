a34341ddbe55d397a9bc5e2c4ee552f5b66c8577
Aleksandar Ristic <storiaca@gmail.com>
2022-08-08 08:54:14 +0200

    Create values in js

---
+++ 26-double-slider/app.js
@@ -0,0 +1,8 @@
+const slideContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
+const slidesLength = slideRight.querySelectorAll("div").length;
+
+console.log(slidesLength);
