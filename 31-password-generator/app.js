93f29d6056540a4a7b37204cae887308497feae9
Aleksandar Ristic <storiaca@gmail.com>
2025-11-08 20:44:39 +0100

    Get random symbol

--- 31-password-generator/app.js
+++ 31-password-generator/app.js
@@ -2,4 +2,14 @@ function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {}
+
 console.log(getRandomLower());
