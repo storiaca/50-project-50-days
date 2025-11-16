d1d5abc391481903f406f58da7b17b016f1604ec
Aleksandar Ristic <storiaca@gmail.com>
2025-11-16 21:12:20 +0100

    Adding Notes to the DOM

--- 33-notes-app/app.js
+++ 33-notes-app/app.js
@@ -1,6 +1,6 @@
 const addBtn = document.getElementById('add');
 
-addBtn.addEventListener('click', () => addNewNote())
+addBtn.addEventListener('click', () => addNewNote('Hello'))
 
 function addNewNote(text = '') {
   const note = document.createElement('div');
@@ -12,6 +12,31 @@ function addNewNote(text = '') {
       <button class="delete"><i class="fas fa-trash-alt"></i></button>
     </div>
     <div class="main ${text ? "" : "hidden"}"></div>
-    <textarea class="${text ? "hidden" : ""}" name="message" id="message"></textarea>
-  `
+    <textarea class="${text ? "hidden" : ""}" name="message"></textarea>
+  `;
+
+  const editBtn = note.querySelector('.edit');
+  const deleteBtn = note.querySelector('.delete');
+  const main = note.querySelector('.main');
+  const textArea = note.querySelector('textarea');
+
+  textArea.value = text
+  main.innerHTML = marked.parse(text)
+
+  deleteBtn.addEventListener('click', () => {
+    note.remove()
+  })
+
+  editBtn.addEventListener('click', () => {
+    main.classList.toggle('hidden')
+    textArea.classList.toggle('hidden')
+  })
+
+  textArea.addEventListener('input', (e) => {
+    const {value} = e.target;
+
+    main.innerHTML = marked.parse(value)
+  })
+
+  document.body.appendChild(note)
 }
