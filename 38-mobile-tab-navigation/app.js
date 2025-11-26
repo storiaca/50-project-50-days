fb8635d6a8cc10217ac1fdb39ab6238430644cc7
Aleksandar Ristic <storiaca@gmail.com>
2025-11-26 11:17:20 +0100

    Navigation effect

--- 38-mobile-tab-navigation/app.js
+++ 38-mobile-tab-navigation/app.js
@@ -1,1 +1,20 @@
+const contents = document.querySelectorAll('.content')
+const listitems = document.querySelectorAll('nav ul li')
 
+listitems.forEach((item, idx) => {
+  item.addEventListener('click', () => {
+    hideAllContents()
+    hideAllItems()
+
+    item.classList.add('active')
+    contents[idx].classList.add('show')
+  })
+})
+
+function hideAllContents() {
+  contents.forEach(content => content.classList.remove('show'))
+}
+
+function hideAllItems() {
+  listitems.forEach(item => item.classList.remove('active'))
+}
