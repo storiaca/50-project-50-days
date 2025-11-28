072c323037ca62ed3b2f9cc577cc9bafd4b284cc
Aleksandar Ristic <storiaca@gmail.com>
2025-11-28 09:02:30 +0100

    Background Position and rotate event

--- 40-3d-background-boxes/app.js
+++ 40-3d-background-boxes/app.js
@@ -1,1 +1,19 @@
+const boxesContainer = document.getElementById('boxes')
+const btn = document.getElementById('btn')
 
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (let y = 0; y < 4; y++) {
+      const box = document.createElement('div')
+      box.classList.add('box')
+      box.style.backgroundPosition = `
+        ${-y * 125}px ${-i * 125}px
+      `
+      boxesContainer.appendChild(box)
+    }  
+  }
+}
+
+createBoxes()
+
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
