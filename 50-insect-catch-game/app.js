90316bea4a46d3ecc9828af3017c5de249742551
Aleksandar Ristic <storiaca@gmail.com>
2025-12-08 17:19:44 +0100

    Insect game functionality start

--- 50-insect-catch-game/app.js
+++ 50-insect-catch-game/app.js
@@ -1,2 +1,30 @@
 const screens = document.querySelectorAll('.screen')
+const chooseInsectBtns = document.querySelectorAll('.choose-insect-btn')
+const startBtn = document.getElementById('start-btn')
+const gameContainer = document.getElementById('game-container')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
 
+let seconds = 0
+let score = 0
+let selectedInsect = {}
+
+startBtn.addEventListener('click', () => screens[0].classList.add('up'))
+
+chooseInsectBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const img = btn.querySelector('img')
+    const src = img.getAttribute('src')
+    const alt = img.getAttribute('alt')
+    selectedInsect = {src, alt}
+    screens[1].classList.add('up')
+    setTimeout(createInsect, 1000)
+    startGame()
+  })
+})
+
+function createInsect() {
+  const insect = document.createElement('div')
+  insect.classList.add('insect')
+}
