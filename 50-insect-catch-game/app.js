6d7580e56b408e322f7edcb122fe0234a6fbbf2a
Aleksandar Ristic <storiaca@gmail.com>
2025-12-09 08:55:42 +0100

    Insect game functionality finish

--- 50-insect-catch-game/app.js
+++ 50-insect-catch-game/app.js
@@ -24,7 +24,59 @@ chooseInsectBtns.forEach(btn => {
   })
 })
 
+function startGame() {
+  setInterval(increaseTime, 1000)
+}
+
+function increaseTime() {
+  let m = Math.floor(seconds / 60)
+  let s = seconds % 60
+
+  m = m < 10 ? `0${m}` : m
+  s = s < 10 ? `0${s}` : s
+
+  timeEl.innerHTML = `Time: ${m}:${s}`
+  seconds++
+}
+
 function createInsect() {
   const insect = document.createElement('div')
   insect.classList.add('insect')
+  const {x,y} = getRandomLocation()
+  insect.style.top = `${y}px`
+  insect.style.left = `${x}px`
+  insect.innerHTML = `<img src="${selectedInsect.src}" alt="${selectedInsect.alt}" style="transform: rotate(${Math.random() * 360}deg)"/>`
+
+  insect.addEventListener('click', catchInsect)
+
+  gameContainer.appendChild(insect)
+}
+
+function getRandomLocation() {
+  const width = window.innerWidth
+  const height = window.innerHeight
+  const x = Math.random() * (width - 200) + 100
+  const y = Math.random() * (height - 200) + 100
+
+  return {x, y}
+}
+
+function catchInsect() {
+  increaseScore()
+  this.classList.add('caught')
+  setTimeout(() => this.remove, 2000)
+  addInsects()
+}
+
+function addInsects() {
+  setTimeout(createInsect, 1000)
+  setTimeout(createInsect, 1500)
+}
+
+function increaseScore() {
+  score++
+  if(score > 19) {
+    message.classList.add('visible')
+  }
+  scoreEl.innerHTML = `Score: ${score}`
 }
