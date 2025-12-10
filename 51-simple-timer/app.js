b08b3f34067a24b33cd9b40cfe4f12d4fb58ea41
Aleksandar Ristic <storiaca@gmail.com>
2025-12-10 10:42:23 +0100

    Timer functionality

--- 51-simple-timer/app.js
+++ 51-simple-timer/app.js
@@ -1,1 +1,65 @@
+const resetBtn = document.getElementById('reset')
+const playBtn = document.getElementById('play')
+const timerEl = document.getElementById('timer')
+const root = document.querySelector(':root')
 
+// Initial setup
+const totalSeconds = 190
+let playing = false
+let currentSeconds = totalSeconds
+timerEl.innerText = formatTime(totalSeconds)
+
+// Run the run() function every second
+const timerInterval = setInterval(run, 1000)
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+  playing = !playing
+  playBtn.classList.toggle('play')
+  playBtn.classList.toggle('bg-green-500')
+  const playIcon = playBtn.querySelector('i')
+  playIcon.classList.toggle('fa-play')
+  playIcon.classList.toggle('fa-pause')
+})
+
+// Run the timer
+function run() {
+  if(playing){
+    currentSeconds -= 1
+    if(currentSeconds <= 0) {
+      clearInterval(timerInterval)
+      resetAll()
+    }
+
+    timerEl.innerText = formatTime(currentSeconds)
+    root.style.setProperty('--degrees', calcDeg())
+  }
+}
+
+// Calculate degrees
+function calcDeg() {
+  return `${360 - (currentSeconds / totalSeconds) * 360}deg`
+}
+
+// Reset timer
+function resetAll() {
+  playing = false;
+  playBtn.classList.remove('play')
+  playBtn.classList.remove('bg-green-500')
+  const playIcon = playBtn.querySelector('i')
+  playIcon.classList.add('fa-play')
+  playIcon.classList.remove('fa-pause')
+  currentSeconds = totalSeconds
+  timerEl.innerText = formatTime(totalSeconds)
+  root.style.setProperty('--degrees', '0deg')
+}
+
+// Format time
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60)
+  const newSeconds = seconds % 60
+
+  return `${minutes.toString().padStart(2, '0')}:${newSeconds.toString().padStart(2, '0')}`
+}
+
+resetBtn.addEventListener('click', resetAll)
